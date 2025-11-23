e71d4f21f9e171d8731fbda032a5ea801ff3de73
syntacsdev <syntaxiomonline@gmail.com>
2025-11-22 18:12:32 -0700

    WIP Student messages page, untested

--- QASystem/src/application/pages/student/StudentMessagesPage.java
+++ QASystem/src/application/pages/student/StudentMessagesPage.java
@@ -1,5 +1,20 @@
 package application.pages.student;
 
+import application.User;
+import databasePart1.DatabaseHelper;
+import javafx.stage.Stage;
+
 public class StudentMessagesPage {
 
+	private final DatabaseHelper database;
+	private final User user;
+	
+	public StudentMessagesPage(DatabaseHelper helper, User user) {
+		this.database = helper;
+		this.user = user;
+	}
+	
+	public void show(Stage primaryStage) {
+		// Back button
+	}
 }
